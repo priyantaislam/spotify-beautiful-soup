374adfbb013835cbf8b8d3dbaa77ceaa024a0635
Priyanta Islam <islam.priyanta@gmail.com>
2023-03-22 22:58:52 +1100

    added main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get("https://www.billboard.com/charts/hot-100/2000-08-12/")
+web_page = response.text
+
+soup = BeautifulSoup(web_page, "html.parser")
