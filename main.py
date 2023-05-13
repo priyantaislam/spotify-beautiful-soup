2cb111e8890942e01e89eb8fa635e3ba8a7248ab
Priyanta Islam <priyantaislam@gmail.com>
2023-05-13 19:24:53 +1000

    reading webpage content

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ import requests
 response = requests.get("https://www.billboard.com/charts/hot-100/2000-08-12/")
 web_page = response.text
 
+#reading webpage content
 soup = BeautifulSoup(web_page, "html.parser")
