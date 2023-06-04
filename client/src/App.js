cdfc977f252282981dc3c88c777afc91bd18a31c
Priyanta Islam <priyantaislam@gmail.com>
2023-06-04 10:44:17 +1000

    refactoring app design

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import DateForm from './Components/DateForm';
-import { useAuthContext } from './Hooks/useAuthContext';
 
 function App() {
-
-  //AUTHENTICATION DETAILS FOR SPOTIFY API
-  const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-  const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
-  const { dispatch } = useAuthContext();
-
-  useEffect(() => {
-    //API Access Token Parameters in request body
-    var authParameters = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
-    };
-
-    //making a POST request to get the spotify api token
-    fetch('https://accounts.spotify.com/api/token', authParameters)
-      .then(result => result.json())
-      //log the token for testing
-      .then(data => dispatch({type: 'TOKEN', payload: data.access_token}));
-  }, []);
-
   return (
     <div>
       <DateForm/>
--- client/src/Components/DateForm.jsx
+++ client/src/Components/DateForm.jsx
@@ -2,16 +2,10 @@ import React, { useState, useEffect, useReducer } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import './DateForm.css'
-import { useAuthContext } from '../Hooks/useAuthContext';
 
 const DateForm = () => {
   const [date, setDate] = useState("0000-00-00");
   const [songs, setSongs] = useState([]);
-  //the song title and artist name for a particular track
-  const [songName, setSongName] = useState("");
-  const [artistName, setArtistName] = useState("");
-  //Grabs the spotify auth token
-  const { token } = useAuthContext();
 
   //changes the date on change
   const handleDateChange = (date) => {
@@ -35,31 +29,6 @@ const DateForm = () => {
     return date >= minDate && date <= maxDate;
   };
 
-  //set spotify query and send api request for search
-  const spotifySearch = (song, artist) => {
-    // Format song and artist for query string
-    const songStr = song.replace(/ /g, '%');
-    const artistStr = artist.replace(/ /g, '%');
-    //Constructing the query string
-    const url = `https://api.spotify.com/v1/search?q=${songStr}%20artist:${artistStr}&type=track&limit=1&offset=0`;
-  
-    fetch(url, {
-      method: 'GET',
-      headers: {
-        'Authorization': 'Bearer ' + token
-      }
-    })
-      .then(response => response.json())
-      .then((data) => {
-        // Handle the response data here
-        
-      })
-      .catch((error) => {
-        // Handle any errors here
-      });
-  };
-  
-
   const handleSubmit = (e) => {
     e.preventDefault(); // Prevents the default form submission behavior
     
@@ -72,21 +41,13 @@ const DateForm = () => {
         .then((response) => response.json())
         .then((data) => {
           // Process the response data
-          console.log(token);
-          setSongs(data.songs);
-          songs.forEach((song) => {
-            // Perform any desired operations with each song
-            setSongName(song[0]);
-            setArtistName(song[1]);
-            //Search for track and add them
-          });
+          console.log(data);
         })
         .catch((error) => {
           // Handle any errors
           console.error(error);
         });
       //Display playlist in embedded window
-      
     }
   };
 
